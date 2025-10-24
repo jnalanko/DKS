6e0c62eee8d9398b47729c051aa7b242d26b18c8
Jarno N. Alanko <alanko.jarno@gmail.com>
2025-10-24 11:21:21 +0300

    WIP

--- src/parallel_queries.rs
+++ src/parallel_queries.rs
@@ -1,4 +1,4 @@
-use std::{cmp::{max, Reverse}, io::Write, ops::Range, path::Path};
+use std::{cmp::{max, min, Reverse}, io::Write, ops::Range, path::Path};
 
 use jseqio::{reader::DynamicFastXReader, seq_db::SeqDB};
 
@@ -9,14 +9,14 @@ struct QueryBatch {
     seqs: SeqDB,
 
     batch_id: usize,
-    sequence_breaks: Vec<usize> // List of nucleotide positions after which we should output a newline in the output
+    sequence_breaks: Vec<usize> // Source sequence changes at these answer indices 
 }
 
 struct ProcessedQueryBatch {
     result: Vec<Option<usize>>,
 
     batch_id: usize,
-    sequence_breaks: Vec<usize> // List of nucleotide positions after which we should output a newline in the output
+    sequence_breaks: Vec<usize> 
 }
 
 impl QueryBatch {
@@ -115,6 +115,7 @@ fn output_thread<W: Write>(query_results: crossbeam::channel::Receiver<Processed
         }
     }
 
+    todo!(); // Need to add a newline after the very last seq
     n_kmers_processed
     // Channel is dropped (= closed) here.
 }
@@ -126,76 +127,103 @@ pub fn lookup_parallel(n_threads: usize, query_path: &Path, index: SingleColored
 
     let mut reader = DynamicFastXReader::from_file(&query_path).unwrap();
     let query_start = std::time::Instant::now();
-    let k = index.k();
 
     let n_kmers_processed = std::thread::scope(|s| {
 
         let reader_handle = s.spawn(|| {
+
             // Reader thread that pushes batches for workers
-            let mut buf = SeqDB::new();
+
+            let mut batch_seqs = SeqDB::new();
+            let mut chars_in_batch = 0_usize;
+            let mut kmers_in_batch = 0_usize;
             let mut seq_breaks = Vec::<usize>::new();
-            let mut n_chars_in_buf = 0_usize;
             let mut batch_id = 0_usize;
+
             while let Some(rec) = reader.read_next().unwrap() {
-                if n_chars_in_buf + rec.seq.len() < batch_size {
-                    buf.push_seq(rec.seq);
-                    n_chars_in_buf += rec.seq.len();
-                    assert!(n_chars_in_buf > 0);
-                    seq_breaks.push(n_chars_in_buf-1);
-                } else {
-                    let mut head_len = batch_size - n_chars_in_buf;
-                    let mut head = &rec.seq[0..head_len];
-                    let mut tail = &rec.seq[head_len..];
-                    loop { // Keep sending full batches for as long as we can
+                // Let b be batch size and n be the length of the sequence.
+                // Split the sequence into m pieces of length b except for the
+                // last sequence that can have a shorter length, such that the
+                // pieces overlap by k-1 characters and cover the whole sequence.
+                // Record a sequence break at the end of the last sequence.
 
-                        buf.push_seq(head);
-                        n_chars_in_buf += head_len;
+                // How many pieces will be there be? That is, what is the smallest
+                // m such that
+                //
+                // b + (m-1)*(b-(k-1)) >= n
+                //
+                // We must have b-k+1 > 0, or otherwise the inequality flips the wrong way around.
+                // Assuming b-k+1 > 0, the solution is:
+                //
+                // m >= (n-k+1) / (b-k+1)
+                //
+                // So we take the ceil of the
 
-                        if tail.len() == 0 {
-                            seq_breaks.push(n_chars_in_buf-1);
-                        }
+                let seq = rec.seq; 
+                let n = seq.len();
+                let b = batch_size;
+                let k = index.k();
 
+                if n < k {
+                    seq_breaks.push(kmers_in_batch);
+                    continue;
+                }
+
+                assert!(b as isize - k as isize + 1 > 0); // b-k+1 > 0
+                let m = (n-k+1).div_ceil(b-k+1);
+                assert!(m > 0); // This should be true since we checked for n < k earlier
+
+                // Sanity checks: 
+                //     if n = k and b > k-1, then m = ceil(1 / positive), so m >= 1. Correct.
+                //     if n = k+3 and b = k, then m = ceil(4) = 4. Correct.
+                //     if n = 100, b = 30 and k = 10, then m = 5. Correct.
+                // Seems to work.
+
+                for piece_idx in 0..m {
+                    let pieces_before = piece_idx;
+                    let start = b*piece_idx - (k-1)*pieces_before;
+                    let piece = if piece_idx < m-1 {
+                        // Not the last piece: has full length b
+                        &rec.seq[start..start+b]
+                    } else {
+                        &rec.seq[start..] // Until the end (can have length shorter than b)
+                    };
+
+
+                    if piece_idx == 0 {
+                        seq_breaks.push(kmers_in_batch);
+                    }
+                    batch_seqs.push_seq(piece);
+                    kmers_in_batch += kmers_in_n(k, piece.len());
+                    chars_in_batch += piece.len();
+
+                    if chars_in_batch >= b {
                         let batch = QueryBatch {
-                            seqs: buf,
+                            seqs: batch_seqs,
                             batch_id,
                             sequence_breaks: seq_breaks,
                         };
+                        batch_send.send(batch).unwrap();
 
-                        batch_send.send(batch);
-
-                        batch_id += 1;
-                        buf = SeqDB::new();
-                        n_chars_in_buf = 0;
+                        // Reset the batch and tracking variables
+                        batch_seqs = SeqDB::new();
                         seq_breaks = Vec::new();
-
-                        if tail.len() >= batch_size {
-                            head = &tail[0..batch_size];
-                            tail = &tail[batch_size..];
-                            head_len = batch_size;
-                        } else {
-                            break;
-                        }
-                    }
-
-                    if tail.len() > 0 {
-                        buf.push_seq(tail);
-                        n_chars_in_buf += tail.len();
-                        seq_breaks.push(n_chars_in_buf-1);
+                        chars_in_batch = 0;
+                        kmers_in_batch = 0;
+                        batch_id += 1;
                     }
                 }
             }
 
-            if n_chars_in_buf > 0 {
-                // Last one
-                seq_breaks.push(n_chars_in_buf - 1);
+            if chars_in_batch > 0 || seq_breaks.len() > 0 {
+                // Push the last remaining non-full batch
                 let batch = QueryBatch {
-                    seqs: buf,
+                    seqs: batch_seqs,
                     batch_id,
                     sequence_breaks: seq_breaks,
                 };
-                batch_send.send(batch);
+                batch_send.send(batch).unwrap();
             }
-
 
             eprintln!("All input read");
             drop(batch_send); // Close the channel
