b9cfa557828ef0319ecfe5545e45f333adac388c
Jarno N. Alanko <alanko.jarno@gmail.com>
2026-03-03 13:58:48 +0200

    1 thread

--- new_benchmark/print_query_commands.py
+++ new_benchmark/print_query_commands.py
@@ -1,5 +1,5 @@
-k_values = list(range(13,63+1,5))
-n_threads = 6
+k_values = list(range(16,63+1,8))
+n_threads = 1
 
 for k in k_values:
-    print(f"/usr/bin/time -v dks lookup -q data/CHM13.fasta.gz -i index/CHM13-k63.dks -t {n_threads} -k {k} 1> out/CHM13-k{k}.tsv 2> logs/CHM13-k{k}.log")
+    print(f"/usr/bin/time -v dks lookup -q data/CHM13.fasta.gz -i index/CHM13-k63.dks -t {n_threads} -k {k} 1> out/CHM13-k{k}-t{n_threads}.tsv 2> logs/CHM13-k{k}-t{n_threads}.log")
