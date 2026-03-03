3f85b54a530dc2a809ce4ffa36f1bbfdcad8b66d
Jarno N. Alanko <alanko.jarno@gmail.com>
2026-03-03 17:10:01 +0200

    Plot

---
+++ new_benchmark/scripts/variable_k_queries.R
@@ -0,0 +1,24 @@
+library(ggplot2)
+
+df <- read.table("results/variable_k_queries.tsv", header = TRUE, sep = "\t")
+
+p <- ggplot(df, aes(x = k, y = query_time_ns_per_bp)) +
+  geom_line() +
+  geom_point(size = 2) +
+  labs(
+    title = "Query time",
+    x = "k",
+    y = "ns / character",
+  ) +
+  theme_minimal(base_size = 14) +
+  theme(
+    panel.grid.minor = element_blank(),
+    panel.grid.major = element_line(color = "grey10", linewidth = 0.2),
+    legend.position = "bottom",
+    plot.title = element_text(hjust = 0.5),
+    axis.title.x = element_text(size = 12),
+    axis.title.y = element_text(size = 12)) +
+  expand_limits(y = 0)
+
+ggsave("results/query_time_vs_k.pdf", p, width = 5, height = 5, dpi = 100, bg = "white")
+print(p)
